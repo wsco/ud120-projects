1b2fe9cf3d1e71ab8ed117b7315668e6d164f597
Wes <wscogg2@wgu.edu>
2017-11-20 13:48:36 -0800

    nb and svm ipynb added
    nb and svm ipynb added

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,3 +1,4 @@
+# %load svm_author_id.py
 #!/usr/bin/python
 
 """ 
@@ -24,7 +25,54 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+def train_and_predict(type, svm, features_train, labels_train, features_test, labels_test):
+    t0 = time()
+    svm.fit(features_train, labels_train)
+    print 'training time with svm''s '+ type +' kernel', time()-t0
+
+    t1 = time()
+    pred = svm.predict(features_test)
+    print 'prediction time with svm''s '+ type +' kernel', time() - t1
+
+    acc = accuracy_score(labels_test, pred)
+    print acc , '\n'
+
+linear_kernel_svm = svm.SVC(kernel = 'linear')    
+train_and_predict('linear', linear_kernel_svm, features_train, labels_train, features_test, labels_test)
+    
+features_train_sample = features_train[:len(features_train)/100]
+labels_train_sample = labels_train[:len(labels_train)/100]
+
+train_and_predict('linear', linear_kernel_svm, features_train_sample,
+                  labels_train_sample, features_test, labels_test)
+    
+rbf_kernel_svm = svm.SVC(kernel = 'rbf')
+train_and_predict('rbf', rbf_kernel_svm, features_train_sample, 
+                  labels_train_sample, features_test, labels_test)
+rbf_kernel_svm = svm.SVC(kernel = 'rbf', C=10)
+train_and_predict('rbf', rbf_kernel_svm, features_train_sample, 
+                  labels_train_sample, features_test, labels_test)
+
+rbf_kernel_svm = svm.SVC(kernel = 'rbf', C=100)
+train_and_predict('rbf', rbf_kernel_svm, features_train_sample, 
+                  labels_train_sample, features_test, labels_test)
+
+rbf_kernel_svm = svm.SVC(kernel = 'rbf', C=1000)
+train_and_predict('rbf', rbf_kernel_svm, features_train_sample, 
+                  \labels_train_sample, features_test, labels_test)
+
+rbf_kernel_svm = svm.SVC(kernel = 'rbf', C=10000)
+train_and_predict('rbf', rbf_kernel_svm, features_train_sample, 
+                  labels_train_sample, features_test, labels_test)
 
 #########################################################
 
+def time_with_power(power, people, times):
+    results = nd.random.power(power, people)
+    for i in range(times):
+        results += nd.random.power(power, 100)
+    return results
 
