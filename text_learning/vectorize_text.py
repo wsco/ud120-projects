32a41f180ba251f8996dddca93e6fbc0a710a039
Wes <wscogg2@wgu.edu>
2017-11-25 17:38:16 -0800

    finish feature selection dev
    finish feature selection dev

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,21 +41,30 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        #temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            content = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            #stop_words = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf", "houectect"]
+            stop_words = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]
+            #stop_words = ["sara", "shackleton", "chris", "germani"]
+            for w in stop_words:
+                content = content.replace(w,"")
+                
             ### append the text to word_data
-
+            word_data.append(content)
+            
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == 'sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
